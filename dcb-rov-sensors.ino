93ed66a1d768f9db72494da78bb1037d4502b1cb
mirman-emmalkan <32554511+mirman-emmalkan@users.noreply.github.com>
2018-04-13 13:51:17 -0700

    Update dcb-rov-sensors.ino

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -11,3 +11,43 @@ void loop()
   light = analogRead(A1);
   Serial.println(light);
 }
+
+#include "MS5837.h"
+
+MS5837 sensor;
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  Serial.println("Starting");
+  
+  Wire.begin();
+
+  sensor.init();
+  
+  sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+}
+
+void loop() {
+
+  sensor.read();
+
+  Serial.print("Pressure: "); 
+  Serial.print(sensor.pressure()); 
+  Serial.println(" mbar");
+  
+  Serial.print("Temperature: "); 
+  Serial.print(sensor.temperature()); 
+  Serial.println(" deg C");
+  
+  Serial.print("Depth: "); 
+  Serial.print(sensor.depth()); 
+  Serial.println(" m");
+  
+  Serial.print("Altitude: "); 
+  Serial.print(sensor.altitude()); 
+  Serial.println(" m above mean sea level");
+
+  delay(1000);
+}
