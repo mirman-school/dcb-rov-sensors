b4d7626c8be8c74b981e79c12104910800c3cd62
mirman-emmalkan <32554511+mirman-emmalkan@users.noreply.github.com>
2018-04-16 13:08:22 -0700

    Update dcb-rov-sensors.ino

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -35,48 +35,6 @@ void loop() {
 }
               
 
-SoftwareSerial myserial(rx, tx);                     
-
-
-String inputstring = "";                             
-String sensorstring = "";                            
-boolean input_string_complete = false;              
-boolean sensor_string_complete = false;              
-float DO;                                           
-
-  if (input_string_complete){                         //if a string from the PC has been received in its entirety
-    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
-    myserial.print('\r');                             //add a <CR> to the end of the string 
-    inputstring = "";                                 //clear the string
-    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
-  }
-
-  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
-    char inchar = (char)myserial.read();              //get the char we just received
-    sensorstring += inchar;                           //add the char to the var called sensorstring
-    if (inchar == '\r') {                             //if the incoming character is a <CR>
-      sensor_string_complete = true;                  //set the flag
-    }
-  }
-
-
-  if (sensor_string_complete== true) {                //if a string from the Atlas Scientific product has been received in its entirety
-    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
-    /*                                                //uncomment this section to see how to convert the DO reading from a string to a float 
-    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
-      DO = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
-      if (DO >= 6.0) {                                //if the DO is greater than or equal to 6.0
-        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
-      }
-      if (DO <= 5.99) {                               //if the DO is less than or equal to 5.99
-        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
-      }
-    }
-    */
-    sensorstring = "";                                //clear the string
-    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
-    sensor.read();
-
   Serial.print("Pressure: "); 
   Serial.print(sensor.pressure(A0)); 
   Serial.println(" mbar");
