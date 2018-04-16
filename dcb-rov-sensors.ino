4ce21e96a23b09045d00a532ec12319b8cb60842
mirman-emmalkan <32554511+mirman-emmalkan@users.noreply.github.com>
2018-04-16 13:03:22 -0700

    Update dcb-rov-sensors.ino

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -1,30 +1,22 @@
 int light; // photoresistor
+#include <SoftwareSerial.h>                          
+#define rx 2(A2)                                     
+#define tx 3(A3)    
+int PTDA
+#include "MS5837.h"
+MS5837 sensor; // pressure sensors
 
 void setup()
 {
   pinMode(A1,INPUT);
-  Serial.begin(9600);
-}
-
-void loop()
-{
-  light = analogRead(A1);
-  Serial.println(light);
-}
-
-
-
-
-
-
-#include "MS5837.h"
-
-MS5837 sensor; // pressure sensors
-
-int PTDA
-
-void setup() {
-  
+  Serial.begin(9600);                             
+  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
+  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+  void serialEvent() {                                  //if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC 
   Serial.begin(9600);
   
   Serial.println("Starting");
@@ -36,25 +28,71 @@ void setup() {
   sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
 }
 
-void loop() {
+void loop() {                                        
+{
+  light = analogRead(A1);
+  Serial.println(light);
+}
+              
 
-  sensor.read();
+SoftwareSerial myserial(rx, tx);                     
+
+
+String inputstring = "";                             
+String sensorstring = "";                            
+boolean input_string_complete = false;              
+boolean sensor_string_complete = false;              
+float DO;                                           
+
+  if (input_string_complete){                         //if a string from the PC has been received in its entirety
+    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
+    myserial.print('\r');                             //add a <CR> to the end of the string 
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+  }
+
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+
+  if (sensor_string_complete== true) {                //if a string from the Atlas Scientific product has been received in its entirety
+    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+    /*                                                //uncomment this section to see how to convert the DO reading from a string to a float 
+    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
+      DO = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
+      if (DO >= 6.0) {                                //if the DO is greater than or equal to 6.0
+        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+      }
+      if (DO <= 5.99) {                               //if the DO is less than or equal to 5.99
+        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+      }
+    }
+    */
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+    sensor.read();
 
   Serial.print("Pressure: "); 
   Serial.print(sensor.pressure(A0)); 
   Serial.println(" mbar");
   
   Serial.print("Temperature: "); 
-  Serial.print(sensor.temperature(A4)); 
+  Serial.print(sensor.temperature(A0)); 
   Serial.println(" deg C");
   
   Serial.print("Depth: "); 
-  Serial.print(sensor.depth(A5)); 
+  Serial.print(sensor.depth(A0)); 
   Serial.println(" m");
   
   Serial.print("Altitude: "); 
-  Serial.print(sensor.altitude(A6)); 
+  Serial.print(sensor.altitude(A0)); 
   Serial.println(" m above mean sea level");
 
   delay(1000);
+  }
 }
