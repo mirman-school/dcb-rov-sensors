0e7be0d325cec6184511c6f217fd0b14c58e6334
mirman-as <32554537+mirman-as@users.noreply.github.com>
2018-04-16 13:21:51 -0700

    Update dcb-rov-sensors.ino

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -1,52 +1,10 @@
-#include "MS5837.h"
-#include <SoftwareSerial.h>                          
-#define rx 2(A2)                                     
-#define tx 3(A3)    
 int light; // photoresistor
 int redlight; // RPR
 int greenlight; // GPR
 int bluelight; //BPR
 int PTDA
 
-MS5837 sensor; // pressure sensors
-
-void setup()
-{
-  pinMode(A1,INPUT);
-  Serial.begin(9600);                             
-  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
-  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
-  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
-  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
-  void serialEvent() {                                  //if the hardware serial port_0 receives a char
-  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
-  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC 
-  Serial.begin(9600);
-  
-  Serial.println("Starting");
-  
-  Wire.begin();
-
-  sensor.init();
-  
-  sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
-}
-
-void loop() {                                        
-{
-  light = analogRead(A1);
-  Serial.println(light);
-  
-   light = analogRead(A7);
-  Serial.println(light);
-  
-   light = analogRead(A);
-  Serial.println(light);
-  
-   light = analogRead(A1);
-  Serial.println(light);
-}
-              
+MS5837 sensor; // pressure sensors 
 
   Serial.print("Pressure: "); 
   Serial.print(sensor.pressure(A0)); 
