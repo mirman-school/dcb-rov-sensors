d1a7d246f0e117a84cf98d9bef24e3d6063305d6
mirman-as <32554537+mirman-as@users.noreply.github.com>
2018-04-16 13:26:01 -0700

    Update dcb-rov-sensors.ino

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -12,8 +12,44 @@ Serial.println(light);
 
 }}
 int redlight; // RPR
+void setup()
+{
+pinMode(A1,INPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+redlight = analogRead(A1);
+Serial.println(redlight);
+
+}}
 int greenlight; // GPR
+void setup()
+{
+pinMode(A2,INPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+greenlight = analogRead(A2);
+Serial.println(greenlight);
+
+}}
 int bluelight; //BPR
+void setup()
+{
+pinMode(A3,INPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+bluelight = analogRead(A3);
+Serial.println(bluelight);
+
+}}
 int PTDA
 
 MS5837 sensor; // pressure sensors 
