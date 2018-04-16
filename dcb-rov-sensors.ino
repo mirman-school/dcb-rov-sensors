3e227d659da3682e291047be5966d13ad079b921
mirman-emmalkan <32554511+mirman-emmalkan@users.noreply.github.com>
2018-04-16 13:27:36 -0700

    Update dcb-rov-sensors.ino

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -10,7 +10,7 @@ void loop()
 light = analogRead(A0);
 Serial.println(light);
 
-}
+}}
 int redlight; // RPR
 void setup()
 {
@@ -23,7 +23,7 @@ void loop()
 redlight = analogRead(A1);
 Serial.println(redlight);
 
-}
+}}
 int greenlight; // GPR
 void setup()
 {
@@ -36,7 +36,7 @@ void loop()
 greenlight = analogRead(A2);
 Serial.println(greenlight);
 
-}
+}}
 int bluelight; //BPR
 void setup()
 {
@@ -49,27 +49,48 @@ void loop()
 bluelight = analogRead(A3);
 Serial.println(bluelight);
 
-}
-int PTDA
+}}
+int pressure
 
 MS5837 sensor; // pressure sensors 
 
+ #include <Wire.h>
+#include "MS5837.h"
+
+MS5837 sensor;
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  Serial.println("Starting");
+  
+  Wire.begin();
+
+  sensor.init();
+  
+  sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+}
+
+void loop() {
+
+  sensor.read();
+
   Serial.print("Pressure: "); 
-  Serial.print(sensor.pressure(A0)); 
+  Serial.print(sensor.pressure()); 
   Serial.println(" mbar");
   
   Serial.print("Temperature: "); 
-  Serial.print(sensor.temperature(A0)); 
+  Serial.print(sensor.temperature()); 
   Serial.println(" deg C");
   
   Serial.print("Depth: "); 
-  Serial.print(sensor.depth(A0)); 
+  Serial.print(sensor.depth()); 
   Serial.println(" m");
   
   Serial.print("Altitude: "); 
-  Serial.print(sensor.altitude(A0)); 
+  Serial.print(sensor.altitude()); 
   Serial.println(" m above mean sea level");
 
   delay(1000);
-  }
 }
