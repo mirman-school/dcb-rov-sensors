d02a7c6dc060315540afe9c13649af10ba9d0e06
mirman_mtaggart <mtaggart@mirman.org>
2018-04-26 20:46:06 -0700

    Add Eric's code

--- dcb-rov-sensors.ino
+++ dcb-rov-sensors.ino
@@ -1,68 +1,83 @@
-// INCLUDES
+#include <AFMotor.h>
 #include <Wire.h>
 #include "MS5837.h"
-#include <SoftwareSerial.h>                          
-#define rx 2                                    
-#define tx 3   
-int light; // photoresistor
-int redlight; // RPR
-int greenlight; // GPR
-int bluelight; //BPR
-MS5837 sensor;
-SoftwareSerial myserial;
 
-// CONSTANTS
-int redPin = A0; // photoresistor
-int greenPin = A1; // RPR
-int bluePin = A2;
-int rgbPin = A3; 
+MS5837 bar;
 
-// VARIABLES
-int redVal;
-int greenVal;
-int blueVal;
-int rgbVal;
-int pressure
+#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
+#define rx 2                                          //define what pin rx is going to be
+#define tx 3                                          //define what pin tx is going to be
 
 
-void setup()
-{
- pinMode(redPin,INPUT);
- pinMode(bluePin,INPUT);
- pinMode(greenPin,INPUT);
- pinMode(redPin,INPUT);
- Serial.begin(9600);
- // the following are for pressure 
- Serial.println("Starting");
- Wire.begin();
- sensor.init();
- sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+float DO;                                             //used to hold a floating point number that is the DO
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);        
+  Wire.begin();
+  
+  bar.init();
+  bar.setFluidDensity(122); // kg/m^3 (997 freshwater, 1029 for seawater)
+  Serial.println("pressure,temperature,depth,altitude,light,dissolvedoxygen");
+}void serialEvent() {                                  //if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC 
 }
 
-void loop()
-{
- redVal = analogRead(redPin);
- greenVal = analogRead(greenPin);
- blueVal = analogRead(bluePin);
- rgbVal = analogRead(rgbPin);
- sensor.read();
- 
- Serial.print(rgbVal);
- Serial.print(",");
- Serial.print(redVal);
- Serial.print(",");
- Serial.print(greenVal);
- Serial.print(",");
- Serial.print(blueVal);
- Serial.print(",");
- Serial.print(sensor.pressure()); 
- Serial.print(",");
- Serial.print(sensor.temperature());
- Serial.print(",");
- Serial.print(sensor.depth()); 
- Serial.print(",");
- Serial.print(sensor.altitude()); 
- Serial.print(",");
- Serial.print("\r");
- delay(1000);
+void loop() {
+
+  if (input_string_complete && millis() % 1000 < 100){                         //if a string from the PC has been received in its entirety
+                   //reset the flag used to tell if we have received a completed string from the PC
+
+    bar.read();
+    Serial.print(bar.pressure()); 
+    Serial.print(",");
+    Serial.print(bar.temperature()); 
+    Serial.print(",");
+    Serial.print(bar.depth()); 
+    Serial.print(",");
+    Serial.print(bar.altitude()); 
+    Serial.print(",");
+    Serial.println(analogRead(A1));
+    Serial.print(",");
+    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
+    myserial.print('\r');                             //add a <CR> to the end of the string 
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;     
+  }
+
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+  if (sensor_string_complete) {                       //if a string from the Atlas Scientific product has been received in its entirety                 //send that string to the PC's serial monitor
+    /*                                                //uncomment this section to see how to convert the DO reading from a string to a float 
+    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
+      DO = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
+      if (DO >= 6.0) {                                //if the DO is greater than or equal to 6.0
+        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+      }
+      if (DO <= 5.99) {                               //if the DO is less than or equal to 5.99
+        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+      }
+    }
+    */
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
+
 }
